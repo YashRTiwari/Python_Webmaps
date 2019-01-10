e7135bda9a05a4466eb99e9b739a2ae355caa847
Yash Rajendra Tiwari <yrtiean@gmail.com>
2019-01-10 07:46:29 +0530

    Added Marker, marker popup

--- Webmap.py
+++ Webmap.py
@@ -1,3 +1,9 @@
-from folium import folium
-map = folium.Map(location = [80,-100])
+import folium
+map = folium.Map(location = [80,-100], zoom_start=6, tiles= "Mapbox Bright")
+
+#adding markers
+map.add_child(folium.Marker(location = [80,-100],
+                            popup = "Marker popup",
+                            icon = folium.Icon(color = "green")))
+
 map.save("map1.html")

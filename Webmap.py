96eed18fd66aac7bfc5417a9d7cba4c9ade83df4
Yash Rajendra Tiwari <yrtiean@gmail.com>
2019-01-10 07:51:45 +0530

    Secondary method to add marker, using FeatureGroup

--- Webmap.py
+++ Webmap.py
@@ -1,9 +1,16 @@
 import folium
 map = folium.Map(location = [80,-100], zoom_start=6, tiles= "Mapbox Bright")
 
-#adding markers
+#adding markers - 1
 map.add_child(folium.Marker(location = [80,-100],
                             popup = "Marker popup",
                             icon = folium.Icon(color = "green")))
+
+#adding markers - 2 Preferred
+feature_grp = folium.FeatureGroup("My Maps")
+feature_grp.add_child(folium.Marker(location = [5,-105],
+                            popup = "Marker popup",
+                            icon = folium.Icon(color = "red")))
+map.add_child(feature_grp)
 
 map.save("map1.html")
